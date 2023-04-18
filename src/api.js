e7d62b5bd2025fb573647192ba738627a60a140a
Yevgeniya Kobrina <yevgeniya@exove.fi>
2023-04-18 16:31:19 +0300

    Functionality improvements

--- src/api.js
+++ src/api.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 
-const SearchImages = async (term) => {
+const SearchImages = async (term, page) => {
 
     const response = await axios.get('https://api.unsplash.com/search/photos', {
         headers: {
             Authorization: 'Client-ID IX0ketCQDU-qZ_OOGYcOgXna95UYk9lz3pLZsZp6igA'
         },
         params: {
-            query: term
+            query: term,
+            per_page: 24
         }
     });
 
